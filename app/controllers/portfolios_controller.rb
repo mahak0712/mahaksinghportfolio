e1c53e941f96142ee7ca36ffb6a17fa6f7cc6ae4
Mahak Singh <singh.maha@husky.neu.edu>
2019-10-30 15:28:58 -0400

    Added show portfolio functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -33,6 +34,10 @@ class PortfoliosController < ApplicationController
         format.html { render :edit } 
       end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
 
