f32889248a900fb72b9f2d2e51da31d9642d0d53
Mahak Singh <singh.maha@husky.neu.edu>
2020-01-22 17:51:10 -0500

    implemented technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,11 @@ end
 
 puts "9 Portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name:"Technology #{technology}"
+  )
+end
+
+puts "3 Technology items created"
+
