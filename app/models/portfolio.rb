20d37a2fd3b58216223b76935d4f591b17c36cca
Mahak Singh <singh.maha@husky.neu.edu>
2020-01-22 16:53:02 -0500

    implemented concerns to generate images across the module

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -10,7 +11,7 @@ class Portfolio < ApplicationRecord
   after_initialize:set_defaults
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/700x300"
-    self.thumb_image ||="http://via.placeholder.com/400x250"
+    self.main_image ||= Placeholder.image_generator(height:'700',width:'300')
+    self.thumb_image ||=Placeholder.image_generator(height:'400',width:'250')
   end
 end
