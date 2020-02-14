32d84115acbc6975f1ed0c576739668f3aaca891
Mahak Singh <singh.maha@husky.neu.edu>
2020-02-14 08:54:12 -0500

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
-  def login_helper
-      if current_user.is_a?(User) 
-       link_to "Logout", destroy_user_session_path, method: :delete 
-      else 
-       (link_to "Sign Up", new_user_registration_path) + 
-"<br>".html_safe +
-       (link_to "Login", new_user_session_path) 
-       end 
+  def login_helper 
+     #  if current_user.is_a?(User)
+     #   (link_to "Register", new_user_registration_path, class: style) +
+     #  " ".html_safe +
+     #  (link_to "Login", new_user_session_path, class: style) 
+       
+     #  else 
+       
+        
+     # link_to "Logout", destroy_user_session_path, method: :delete, class: style
+     #   end 
   end
 
   def source_helper(layout_name)
@@ -14,5 +17,9 @@ module ApplicationHelper
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
+  end
+
+  def copyright_generator
+     MahakSinghViewTool::Renderer.copyright 'Mahak Singh', 'All rights reserved'
   end
 end
