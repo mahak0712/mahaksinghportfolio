53de4604c1cc0bb206efd174aa767ef0bd1169e6
Mahak Singh <singh.maha@husky.neu.edu>
2020-02-18 12:06:41 -0500

    order

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.order("position ASC")
   end
 
   # def sort
