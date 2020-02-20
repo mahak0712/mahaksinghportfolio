3221a15f0af7b2c56f225c1b99d567e93d6359d8
Mahak Singh <singh.maha@husky.neu.edu>
2020-02-19 23:56:15 -0500

    implemented image upload

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,16 @@
 module PortfoliosHelper
+def image_generator(height:, width:)
+    "http://via.placeholder.com/#{height}x#{width}"
+  end
+
+
+def portfolio_img img, type
+    if !img.nil?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    elsif 
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
