688d77e8720f36cc870b2e67794031b519498c49
Mahak Singh <singh.maha@husky.neu.edu>
2020-05-09 00:04:50 -0700

    removed url loader from environmentjs file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -30,12 +30,12 @@ const webpack = require('webpack');
 
 
 // resolve-url-loader must be used before sass-loader
-environment.loaders.get('sass').use.splice(-1, 0, {
-    loader: 'resolve-url-loader',
-    options: {
-        attempts: 1
-    }
-});
+// environment.loaders.get('sass').use.splice(-1, 0, {
+//     loader: 'resolve-url-loader',
+//     options: {
+//         attempts: 1
+//     }
+// });
 
 
 // Add an additional plugin of your choosing : ProvidePlugin
