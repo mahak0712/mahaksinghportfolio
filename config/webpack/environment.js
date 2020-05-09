3288d819bb78b072e450051ef6e1a5d9beb61a74
Mahak Singh <singh.maha@husky.neu.edu>
2020-05-09 02:26:21 -0700

    added to package.json

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -31,7 +31,7 @@ const webpack = require('webpack');
 
 // resolve-url-loader must be used before sass-loader
 environment.loaders.get('sass').use.splice(-1, 0, {
-    loader: 'resolve-url',
+    loader: 'resolve-url-loader',
     options: {
         attempts: 1
     }
