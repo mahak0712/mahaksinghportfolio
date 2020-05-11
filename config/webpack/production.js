f21cac88582fc1966b18285e9fee8121359f8111
Mahak Singh <singh.maha@husky.neu.edu>
2020-05-10 17:45:26 -0700

    added debug
    Signed-off-by: Mahak Singh <singh.maha@husky.neu.edu>

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+config.log_level = debug
 module.exports = environment.toWebpackConfig()
